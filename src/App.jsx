47d4ecdb0c069ae2a41b2428309448ca0320cd32
sayyedulawwab <38081373+sayyedulawwab@users.noreply.github.com>
2022-03-16 13:33:59 +0600

    ADD: header and the dropdown menu of countries

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,46 @@
+import { FormControl, MenuItem, Select } from '@mui/material';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide');
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch('https://disease.sh/v3/covid-19/countries')
+        .then(response => response.json())
+        .then(data => {
+          const countries = data.map(country => ({
+            name: country.country, // United States, United Kingdom, France
+            value: country.countryInfo.iso2, // UK, USA, FR
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
+  const handleCountryChange = event => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+  };
   return (
     <div className="app">
-    
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app__dropdown">
+          <Select
+            variant="outlined"
+            value={country}
+            onChange={handleCountryChange}
+          >
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            {countries.map(country => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
